48ccd49a91b7eeaece6630c2a757d7518b4ea5b4
dryckman2 <dryckman2@huskers.unl.edu>
2024-04-24 17:49:24 -0500

    Server Client Setup

--- python/client.py
+++ python/client.py
@@ -1,10 +1,13 @@
+import secrets
 import socket
+import traceback
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
-CLIENT_PRIVATE = RSA.importKey("""-----BEGIN RSA PRIVATE KEY-----
+CLIENT_PRIVATE = RSA.importKey(
+    """-----BEGIN RSA PRIVATE KEY-----
 MIICXQIBAAKBgQCeg9uNM3cb1Z1kidvG+OoyO2SzdbCpQmKlWEbi4HRADzQnRLy3
 9TWBr/QQ///oVZT/sLhBArmHEN7U1Hm+pk/uvnvndDdbkeZ95HmhekT/Gcs5JIDB
 ehkoI+Nh4k10zf6XqBO34XpnUB6KN44S5b9o1SFKstwYpC4XAoVXEOGh8QIDAQAB
@@ -18,15 +21,19 @@ Cd1uEgmXU0tMlVc9aQtoz84Icct6ep29ym6ZBOJjizckDV/qcszqutjxaqsu+2xo
 MQmQg0/oG6FYGWd0bAnqnz0beAwB1KsAIpU57cAZD00/2fmLwlsILCBkreLcsH/d
 CO0N5nO5CVWCVKKfulECQQCuuQped0DjsO5EH4bTonEzObwVj+Kz001P5XjLyS7o
 e9GWjgcXvt8n8qLUnLMw4dc0/BXlBO3RIteM5kOLpwBK
------END RSA PRIVATE KEY-----""")
+-----END RSA PRIVATE KEY-----"""
+)
 
 HOST = "127.0.0.1"
-PORT = 65432
+PORT = 65431
+
+BLOCK_SIZE = AES.block_size
+MAX_RECV = 4028
 
 
 def load_public_keys():
-    server_pub_f = open('server_public.pem', 'rb')
-    client_pub_f = open('client_public.pem', 'rb')
+    server_pub_f = open("server_public.pem", "rb")
+    client_pub_f = open("client_public.pem", "rb")
     server_pub_key = RSA.importKey(server_pub_f.read())
     client_pub_key = RSA.importKey(client_pub_f.read())
 
@@ -40,34 +47,40 @@ def main():
     client_rsa_decrypter = PKCS1_OAEP.new(CLIENT_PRIVATE)
     server_rsa_encrypter = PKCS1_OAEP.new(server_public)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
+        try:
+            conn.connect((HOST, PORT))
 
-        # Receive encrypted information from host
-        enc_aes_key = s.recv(1024)
-        server_aes_nonce_enc = s.recv(1024)
-        print("AES Key Received")
+            # Setup server connection
+            n1 = secrets.token_urlsafe(16)
+            conn.sendall(server_rsa_encrypter.encrypt(n1.encode()))
 
-        # Use information to create aes encrypter and decrypter
-        aes_key = client_rsa_decrypter.decrypt(enc_aes_key)
-        aes_cipher_enc = AES.new(aes_key, AES.MODE_CTR)
-        server_aes_nonce = client_rsa_decrypter.decrypt(server_aes_nonce_enc)
-        aes_cipher_denc = AES.new(aes_key, AES.MODE_CTR,
-                                  nonce=server_aes_nonce)
+            n1x = client_rsa_decrypter.decrypt(conn.recv(MAX_RECV)).decode()
+            if n1 == n1x:
+                print("Client Accepts Server...")
+            else:
+                print("Client Rejects Server.")
+                raise Exception("Client Rejects")
 
-        # Send client encrypted nonce to server for decoding
-        nonce = aes_cipher_enc.nonce
-        enc_nonce = server_rsa_encrypter.encrypt(nonce)
-        s.sendall(enc_nonce)
+            enc_server_key = conn.recv(128)  # Receive just key
+            server_aes_key = client_rsa_decrypter.decrypt(enc_server_key)
+            server_aes_encrypter = AES.new(
+                server_aes_key, AES.MODE_CTR, nonce=n1.encode()[:15]
+            )
+            server_aes_decrypter = AES.new(
+                server_aes_key, AES.MODE_CTR, nonce=n1.encode()[:15]
+            )
+            n2 = server_aes_encrypter.encrypt(
+                server_aes_decrypter.decrypt(conn.recv(MAX_RECV))
+            )
+            conn.sendall(n2)
 
-        print("Messages\n-------------------------------------")
-        while True:
-            message = input("You: ")
-            enc_message = aes_cipher_enc.encrypt(pad(message.encode(), AES.block_size))
-            s.sendall(enc_message)
-            enc_server_message = s.recv(1024)  # Await response
-            server_message = unpad(aes_cipher_denc.decrypt(enc_server_message), AES.block_size).decode()
-            print(f"Server: {server_message}")
+            print("\n\n-=-=-=-=-=-=-=-=-=-=-=-=-=-\nClient Term")
+
+        except Exception as e:
+            print(traceback.format_exc())
+        finally:
+            conn.close()
 
 
 if __name__ == "__main__":
--- python/server.py
+++ python/server.py
@@ -1,4 +1,8 @@
+import json
+import secrets
 import socket
+import struct
+import traceback
 
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
@@ -6,7 +10,8 @@ from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
-SERVER_PRIVATE = RSA.importKey("""-----BEGIN RSA PRIVATE KEY-----
+SERVER_PRIVATE = RSA.importKey(
+    """-----BEGIN RSA PRIVATE KEY-----
 MIICXQIBAAKBgQDo8mtIodJtwTN14XBC8uNCmArG3uJtNASN+Kn2GsdjRJqEx1ED
 nMCRFm9Z4ZEYtutQbHtwsqHuk+RGu92+mFs+mUBfll4FYmB4yaMmKw08Ho5IyJbp
 cK9bV0Lb8Ff5Zr4KdUKVQDMDRiUYHf/OTiytI28dGhsh3HgTo5QkRkCgOwIDAQAB
@@ -20,15 +25,19 @@ mlYfs18741fiz8bPyRAxQwvaG+NXJ2w8U/SEsVKRkcRe1ob78Pw51Sp7TQJBAN2D
 ifHE+pYySVEGermRsFh9vO79MkgyNLJbeaeSixiZhPhivnV7XiXLfAENcHTihifK
 /MoAdZv3Z4+7LAQ2W80CQQCHlQOxpXl0CvEq1OXotw82L+8DQhaONz8VzF2JL/Yg
 EhP//e4oJcBokB7A+unm2M/XYGLaKD+7wOV+dUr1oqfB
------END RSA PRIVATE KEY-----""")
+-----END RSA PRIVATE KEY-----"""
+)
 
 HOST = "127.0.0.1"
-PORT = 65432
+PORT = 65431
+
+BLOCK_SIZE = AES.block_size
+MAX_RECV = 4028
 
 
 def load_public_keys():
-    server_pub_f = open('server_public.pem', 'rb')
-    client_pub_f = open('client_public.pem', 'rb')
+    server_pub_f = open("server_public.pem", "rb")
+    client_pub_f = open("client_public.pem", "rb")
     server_pub_key = RSA.importKey(server_pub_f.read())
     client_pub_key = RSA.importKey(client_pub_f.read())
 
@@ -46,40 +55,42 @@ def main():
         s.bind((HOST, PORT))
         s.listen()
         conn, add = s.accept()
-        print("Client Found")
+        print("Client Found...")
         with conn:
-            while True:
-                # Generate New aes Key and Nonce
-                aes_key = get_random_bytes(16)
-                aes_cipher_enc = AES.new(aes_key, AES.MODE_CTR)
-                nonce = aes_cipher_enc.nonce
+            try:
+                # Setup client connection
+                msg = conn.recv(MAX_RECV)
+                nx = server_rsa_decrypter.decrypt(msg)
+                client_aes_key = get_random_bytes(16)
+                client_aes_encrypt = AES.new(
+                    client_aes_key, AES.MODE_CTR, nonce=nx[:15]
+                )  # AES using the first 16 bytes of n1 as nonce
+                client_aes_decrypt = AES.new(
+                    client_aes_key, AES.MODE_CTR, nonce=nx[:15]
+                )  # AES using the first 16 bytes of n1 as nonce
+                conn.sendall(client_rsa_encryptor.encrypt(nx))
 
-                # Encrypt aes Key and Nonce with RSA
-                enc_aes_key = client_rsa_encryptor.encrypt(aes_key)
-                enc_nonce = client_rsa_encryptor.encrypt(nonce)
+                msg = client_rsa_encryptor.encrypt(client_aes_key)
+                conn.sendall(msg)
+                conn.recv(0)
+                n2 = secrets.token_urlsafe(16)
+                conn.sendall(client_aes_encrypt.encrypt(n2.encode()))
 
-                # Send to client
-                conn.sendall(enc_aes_key)
-                conn.sendall(enc_nonce)
+                n2x = client_aes_decrypt.decrypt(conn.recv(MAX_RECV)).decode()
+                if n2 == n2x:
+                    print("Server Accepts Client...")
+                else:
+                    print(f"{n2=}")
+                    print(f"{n2x=}")
+                    print("Server Rejects Client.")
+                    raise Exception("Server Rejects")
 
-                # Receive client's nonce
-                client_aes_nonce_enc = conn.recv(1024)
+                print("\n\n-=-=-=-=-=-=-=-=-=-=-=-=-\nServer Term")
 
-                # Use decrypted nonce to make a decrypter for client messages
-                client_aes_nonce = server_rsa_decrypter.decrypt(client_aes_nonce_enc)
-                aes_cipher_denc = AES.new(aes_key, AES.MODE_CTR,
-                                          nonce=client_aes_nonce)
-                print("Messages\n-------------------------------------")
-                while True:
-                    enc_client_message = conn.recv(1024)  # Await response
-                    if not enc_client_message:
-                        # Client Left
-                        break
-                    client_message = unpad(aes_cipher_denc.decrypt(enc_client_message), AES.block_size).decode()
-                    print(f"Client: {client_message}")
-                    message = input("You: ")
-                    enc_message = aes_cipher_enc.encrypt(pad(message.encode(), AES.block_size))
-                    conn.sendall(enc_message)
+            except Exception as e:
+                print(traceback.format_exc())
+            finally:
+                conn.close()
 
 
 if __name__ == "__main__":
